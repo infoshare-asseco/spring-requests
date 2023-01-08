bda290eec38d96e6e0f9242cefc3de2b671da486
maciej.koziara <maciej.koziara@airhelp.com>
2023-01-08 11:48:51 +0100

    Solution 1

--- src/main/java/pl/infoshare/requests/vehicles/VehicleRepository.java
+++ src/main/java/pl/infoshare/requests/vehicles/VehicleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 import pl.infoshare.requests.vehicles.model.Vehicle;
-import pl.infoshare.requests.vehicles.model.VehicleType;
 import pl.infoshare.requests.vehicles.model.VehicleUpdateRequest;
 
 import javax.annotation.PostConstruct;
@@ -13,23 +12,25 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+
+import static pl.infoshare.requests.vehicles.model.Vehicle.bus;
+import static pl.infoshare.requests.vehicles.model.Vehicle.tram;
 
 @Repository
 public class VehicleRepository {
-    private final static AtomicInteger idGenerator = new AtomicInteger(1);
+
     private final List<Vehicle> vehicles = new ArrayList<>();
 
     @PostConstruct
     void init() {
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.BUS, BigDecimal.valueOf(100000), "GTC001", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(90000), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.BUS, BigDecimal.valueOf(15000), "GTC002", LocalDate.of(2020, Month.JANUARY, 20), BigDecimal.valueOf(10000), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.BUS, BigDecimal.valueOf(150000), "GTC003", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(110000), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.TRAM, BigDecimal.valueOf(200000.23), "GTC004", LocalDate.of(2021, Month.FEBRUARY, 28), BigDecimal.valueOf(190000.11), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.TRAM, BigDecimal.valueOf(210000), "GTC005", LocalDate.of(2020, Month.OCTOBER, 1), BigDecimal.valueOf(195000), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.TRAM, BigDecimal.valueOf(11000), "GTC006", LocalDate.of(2021, Month.MAY, 1), BigDecimal.valueOf(1000), "Tczew"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.BUS, BigDecimal.valueOf(100000), "WAW001", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(90000), "Warszawa"));
-        vehicles.add(new Vehicle(idGenerator.getAndIncrement(), VehicleType.TRAM, BigDecimal.valueOf(100000), "WAW002", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(60000), "Warszawa"));
+        vehicles.add(bus(BigDecimal.valueOf(100000), "GDA001", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(90000), "Gdańsk"));
+        vehicles.add(bus(BigDecimal.valueOf(15000), "GDA002", LocalDate.of(2020, Month.JANUARY, 20), BigDecimal.valueOf(10000), "Gdańsk"));
+        vehicles.add(bus(BigDecimal.valueOf(150000), "GDA003", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(110000), "Gdańsk"));
+        vehicles.add(tram(BigDecimal.valueOf(200000.23), "GDA004", LocalDate.of(2021, Month.FEBRUARY, 28), BigDecimal.valueOf(190000.11), "Gdańsk"));
+        vehicles.add(tram(BigDecimal.valueOf(210000), "GDA005", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(195000), "Gdańsk"));
+        vehicles.add(tram(BigDecimal.valueOf(11000), "GDA006", LocalDate.of(2021, Month.MAY, 1), BigDecimal.valueOf(1000), "Gdańsk"));
+        vehicles.add(bus(BigDecimal.valueOf(100000), "WAW001", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(90000), "Warszawa"));
+        vehicles.add(tram(BigDecimal.valueOf(100000), "WAW002", LocalDate.of(2021, Month.MARCH, 1), BigDecimal.valueOf(90000), "Warszawa"));
     }
 
     public List<Vehicle> findAll() {
@@ -37,7 +38,7 @@ public class VehicleRepository {
     }
 
     public void save(Vehicle vehicle) {
-        vehicles.add(vehicle.withId(idGenerator.getAndIncrement()));
+        vehicles.add(vehicle);
     }
 
     public void update(Integer id, VehicleUpdateRequest updateRequest) {
--- src/main/java/pl/infoshare/requests/vehicles/model/Vehicle.java
+++ src/main/java/pl/infoshare/requests/vehicles/model/Vehicle.java
@@ -5,11 +5,13 @@ import lombok.With;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Value
 public class Vehicle {
-    @With
-    Integer id;
+    private static AtomicInteger idGenerator = new AtomicInteger(1);
+
+    Integer id = idGenerator.getAndIncrement();
     VehicleType type;
     BigDecimal mileage;
     String registrationNumber;
@@ -18,4 +20,12 @@ public class Vehicle {
     @With
     BigDecimal lastReviewMileage;
     String city;
+
+    public static Vehicle bus(BigDecimal mileage, String registrationNumber, LocalDate lastReviewDate, BigDecimal lastReviewMileage, String city) {
+        return new Vehicle(VehicleType.BUS, mileage, registrationNumber, lastReviewDate, lastReviewMileage, city);
+    }
+
+    public static Vehicle tram(BigDecimal mileage, String registrationNumber, LocalDate lastReviewDate, BigDecimal lastReviewMileage, String city) {
+        return new Vehicle(VehicleType.TRAM, mileage, registrationNumber, lastReviewDate, lastReviewMileage, city);
+    }
 }
